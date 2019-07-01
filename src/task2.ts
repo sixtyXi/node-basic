dd43648543fdcd489e847d619759a92d9f5ea853
sixtyXi <sixty.xi@gmail.com>
2019-07-01 21:45:08 +0400

    Transform headers to lower case

--- src/task2.ts
+++ src/task2.ts
@@ -2,31 +2,26 @@ import csv from 'csvtojson';
 import fs from 'fs';
 import { pipeline } from 'stream';
 
-const dir = './dist';
+import { outputDir, inputPath, outputPath, converterOptions } from './task2-config';
 
-if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir);
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir);
 }
 
-const csvFilePath = './scv/input_example.csv';
-const outputPath = `${dir}/output_example.txt`;
-
-const readStream = fs.createReadStream(csvFilePath);
+const readStream = fs.createReadStream(inputPath);
 const writeStream = fs.createWriteStream(outputPath);
-const converterOptions = {
-  noheader: false,
-  headers: ['book', 'author', 'amount', 'price'],
-  ignoreColumns: /amount/,
-  checkType: true,
-};
+const converter = csv(converterOptions).preFileLine((data, lineIdx): string =>
+  lineIdx === 0 ? data.toLowerCase() : data
+);
 
 const errorHandler = (error: Error): void => console.error('Pipeline failed.', error);
 const successHandler = (): void =>
   console.log(`All writes are now complete. File's path is ${outputPath}`);
 
-pipeline(
-  readStream,
-  csv(converterOptions).subscribe((json): PromiseLike<void> => json),
-  writeStream,
-  (err): void => (err ? errorHandler(err) : successHandler())
-);
+pipeline(readStream, converter, writeStream, (err): void => {
+  if (err) {
+    errorHandler(err);
+  } else {
+    successHandler();
+  }
+});
